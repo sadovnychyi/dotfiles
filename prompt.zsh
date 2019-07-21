4229192c1c4ddfd6bba629825e1ae32c80829261
Dmytro Sadovnychyi <git@dmit.ro>
2019-07-21 21:33:23 +0800

    Show venv in rprompt

--- prompt.zsh
+++ prompt.zsh
@@ -33,7 +33,7 @@ fi
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
-    # virtualenv            # python virtual environment (https://docs.python.org/3/library/venv.html)
+    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     # anaconda              # conda environment (https://conda.io/)
     # pyenv                 # python environment (https://github.com/pyenv/pyenv)
     # nodenv                # node.js version from nodenv (https://github.com/nodenv/nodenv)
