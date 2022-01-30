b1644c2c6cbd96106ffee6649e8ff8dedd9e906a
Dmytro Sadovnychyi <git@dmit.ro>
2022-01-30 17:55:29 +0800

    Update SSL gen script

--- localhost-ssl.sh
+++ localhost-ssl.sh
@@ -2,10 +2,11 @@
 
 # https://letsencrypt.org/docs/certificates-for-localhost/
 # This will create a certificate for "localhost".
-openssl req -x509 -out /Users/sadovnychyi/.localhost.crt -keyout /Users/sadovnychyi/.localhost.key \
+openssl req -x509 -out ~/.localhost.crt -keyout ~/.localhost.key \
   -newkey rsa:2048 -nodes -sha256 \
   -days 999 \
   -subj '/CN=localhost' -extensions EXT -config <( \
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 # Mark created certificate as "trusted" on this machine.
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /Users/sadovnychyi/.localhost.crt
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ~/.localhost.crt
+brew services restart nginx
