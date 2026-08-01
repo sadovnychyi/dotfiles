f7e7a6be88c0a3de40735307e5040ed03af818c1
Dmytro Sadovnychyi <git@dmit.ro>
2026-08-01 23:22:50 +0200

    Support local HTTPS subdomains

--- localhost-ssl.sh
+++ localhost-ssl.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # https://letsencrypt.org/docs/certificates-for-localhost/
-# This will create a certificate for "localhost".
+# This will create a certificate for localhost and its development subdomains.
 openssl req -x509 -out ~/.localhost.crt -keyout ~/.localhost.key \
   -newkey rsa:2048 -nodes -sha256 \
   -days 999 \
   -subj '/CN=localhost' -extensions EXT -config <( \
-   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost,DNS:contracts.localhost,DNS:*.contracts.localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 # Mark created certificate as "trusted" on this machine.
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ~/.localhost.crt
 brew services restart nginx
